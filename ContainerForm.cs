7ff0a99015aa0a4f5a76b577c1d4d9364b3eef14
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-08-07 09:37:40 +0200

    move stats to dict so they can be reused

--- ContainerForm.cs
+++ ContainerForm.cs
@@ -169,6 +169,72 @@ namespace osu_stats
             return string.Format("{0:#,##0}h {1:D2}m", (int)t.TotalHours, t.Minutes);
         }
 
+        private Dictionary<string, string> GetStats() {
+            var dict = new Dictionary<string, string>();
+            var settings = Settings.Load();
+            if (settings.NewJson == null || settings.OldJson == null)
+                return dict;
+            var mode = settings.DefaultGameMode;
+            var response_new = JsonConvert.DeserializeObject<UserInfo>(settings.NewJson);
+            var response_old = JsonConvert.DeserializeObject<UserInfo>(settings.OldJson);
+            var new_stats = ReorganiseStats(response_new);
+            var old_stats = ReorganiseStats(response_old);
+            var playtimeoffset = mode == 1 ? settings.StdRxPlayTimeOffset : 0;
+            dict["global_rank"] = FormatNumber(new_stats[mode].GlobalLeaderboardRank);
+            dict["global_rank_change"] = FormatGainInt(new_stats[mode].GlobalLeaderboardRank, old_stats[mode].GlobalLeaderboardRank, true);
+            dict["country_rank"] = FormatNumber(new_stats[mode].CountryLeaderboardRank);
+            dict["country_rank_change"] = FormatGainInt(new_stats[mode].CountryLeaderboardRank, old_stats[mode].CountryLeaderboardRank, true);
+            dict["ranked_score"] = FormatNumber(new_stats[mode].RankedScore);
+            dict["ranked_score_change"] = FormatGainInt((int)new_stats[mode].RankedScore, (int)old_stats[mode].RankedScore);
+            dict["total_score"] = FormatNumber(new_stats[mode].TotalScore);
+            dict["total_score_change"] = FormatGainInt((int)new_stats[mode].TotalScore, (int)old_stats[mode].TotalScore);
+            dict["pp"] = FormatNumber(new_stats[mode].Pp);
+            dict["pp_change"] = FormatGainInt((int)new_stats[mode].Pp, (int)old_stats[mode].Pp);
+            dict["accuracy"] = FormatAccuracy(new_stats[mode].Accuracy);
+            dict["accuracy_change"] = FormatAccuracyGain(new_stats[mode].Accuracy, old_stats[mode].Accuracy);
+            dict["playtime"] = FormatPlayTime(new_stats[mode].Playtime - playtimeoffset);
+            dict["playtime_change"] = FormatPlayTimeGain(new_stats[mode].Playtime, old_stats[mode].Playtime);
+            dict["playcount"] = FormatNumber(new_stats[mode].Playcount);
+            dict["playcount_change"] = FormatGainInt((int)new_stats[mode].Playcount, (int)old_stats[mode].Playcount);
+            dict["total_hits"] = FormatNumber(new_stats[mode].TotalHits);
+            dict["total_hits_change"] = FormatGainInt((int)new_stats[mode].TotalHits, (int)old_stats[mode].TotalHits);
+            dict["clears"] = FormatNumber(settings.Clears[mode]);
+            dict["clears_change"] = FormatGainInt(settings.Clears[mode], settings.ClearsOld[mode]);
+            if (settings.ScoreRank != null) {
+                dict["score_rank"] = FormatNumber(settings.ScoreRank[mode]);
+                dict["score_rank_change"] = FormatGainInt(settings.ScoreRank[mode], settings.ScoreRankOld[mode]);
+            }
+            if (settings.Firsts != null && settings.FirstsOld != null) {
+                dict["firsts"] = FormatNumber(settings.Firsts[mode]);
+                dict["firsts_change"] = FormatGainInt(settings.Firsts[mode], settings.FirstsOld[mode]);
+            }
+            if (settings.NextScoreRank != null) {
+                dict["next_rank_score"] = FormatNumber(settings.NextScoreRank[mode]);
+                dict["next_rank_score_needed"] = FormatNumber(settings.NextScoreRank[mode] - new_stats[mode].RankedScore);
+            }
+            if (settings.NextPPRank != null) {
+                dict["next_rank_pp"] = FormatNumber(settings.NextPPRank[mode]);
+                dict["next_rank_pp_needed"] = FormatNumber(settings.NextPPRank[mode] - new_stats[mode].Pp);
+            }
+            dict["ssh"] = FormatNumber(new_stats[mode].Grades.XhCount);
+            dict["ssh_change"] = FormatGainInt(new_stats[mode].Grades.XhCount, old_stats[mode].Grades.XhCount);
+            dict["ss"] = FormatNumber(new_stats[mode].Grades.XCount);
+            dict["ss_change"] = FormatGainInt(new_stats[mode].Grades.XCount, old_stats[mode].Grades.XCount);
+            dict["sh"] = FormatNumber(new_stats[mode].Grades.ShCount);
+            dict["sh_change"] = FormatGainInt(new_stats[mode].Grades.ShCount, old_stats[mode].Grades.ShCount);
+            dict["s"] = FormatNumber(new_stats[mode].Grades.SCount);
+            dict["s_change"] = FormatGainInt(new_stats[mode].Grades.SCount, old_stats[mode].Grades.SCount);
+            dict["a"] = FormatNumber(new_stats[mode].Grades.ACount);
+            dict["a_change"] = FormatGainInt(new_stats[mode].Grades.ACount, old_stats[mode].Grades.ACount);
+            dict["b"] = FormatNumber(new_stats[mode].Grades.BCount);
+            dict["b_change"] = FormatGainInt(new_stats[mode].Grades.BCount, old_stats[mode].Grades.BCount);
+            dict["c"] = FormatNumber(new_stats[mode].Grades.CCount);
+            dict["c_change"] = FormatGainInt(new_stats[mode].Grades.CCount, old_stats[mode].Grades.CCount);
+            dict["d"] = FormatNumber(new_stats[mode].Grades.DCount);
+            dict["d_change"] = FormatGainInt(new_stats[mode].Grades.DCount, old_stats[mode].Grades.DCount);
+            return dict;
+        }
+
         private void ReloadStats() {
             var settings = Settings.Load();
             if (settings.NewJson == null || settings.OldJson == null)
@@ -176,63 +242,59 @@ namespace osu_stats
             var modes = new string[] { "Standard", "Standard RX", "Standard AP", "Taiko", "Taiko RX", "Ctb", "Ctb RX", "Mania" };
             var mode = settings.DefaultGameMode;
             var response_new = JsonConvert.DeserializeObject<UserInfo>(settings.NewJson);
-            var response_old = JsonConvert.DeserializeObject<UserInfo>(settings.OldJson);
-            var new_stats = ReorganiseStats(response_new);
-            var old_stats = ReorganiseStats(response_old);
-            var playtimeoffset = mode == 1 ? settings.StdRxPlayTimeOffset : 0;
-            GlobalRankBox.Text = FormatNumber(new_stats[mode].GlobalLeaderboardRank);
-            GlobalRankGainBox.Text = FormatGainInt(new_stats[mode].GlobalLeaderboardRank, old_stats[mode].GlobalLeaderboardRank, true);
-            CountryRankBox.Text = FormatNumber(new_stats[mode].CountryLeaderboardRank);
-            CountryRankGainBox.Text = FormatGainInt(new_stats[mode].CountryLeaderboardRank, old_stats[mode].CountryLeaderboardRank, true);
-            RankedScoreBox.Text = FormatNumber(new_stats[mode].RankedScore);
-            RankedScoreGainBox.Text = FormatGainInt((int)new_stats[mode].RankedScore, (int)old_stats[mode].RankedScore);
-            TotalScoreBox.Text = FormatNumber(new_stats[mode].TotalScore);
-            TotalScoreGainBox.Text = FormatGainInt((int)new_stats[mode].TotalScore, (int)old_stats[mode].TotalScore);
-            PpBox.Text = FormatNumber(new_stats[mode].Pp);
-            PpGainBox.Text = FormatGainInt((int)new_stats[mode].Pp, (int)old_stats[mode].Pp);
-            AccuracyBox.Text = FormatAccuracy(new_stats[mode].Accuracy);
-            AccuracyGainBox.Text = FormatAccuracyGain(new_stats[mode].Accuracy, old_stats[mode].Accuracy);
-            PlayTimeBox.Text = FormatPlayTime(new_stats[mode].Playtime - playtimeoffset);
-            PlayTimeGainBox.Text = FormatPlayTimeGain(new_stats[mode].Playtime, old_stats[mode].Playtime);
-            PlayCountBox.Text = FormatNumber(new_stats[mode].Playcount);
-            PlayCountGainBox.Text = FormatGainInt((int)new_stats[mode].Playcount, (int)old_stats[mode].Playcount);
-            TotalHitsBox.Text = FormatNumber(new_stats[mode].TotalHits);
-            TotalHitsGainBox.Text = FormatGainInt((int)new_stats[mode].TotalHits, (int)old_stats[mode].TotalHits);
-            ClearsBoxGain.Text = FormatGainInt((int)new_stats[mode].TotalHits, (int)old_stats[mode].TotalHits);
-            ClearsBox.Text = FormatNumber(settings.Clears[mode]);
-            ClearsBoxGain.Text = FormatGainInt(settings.Clears[mode], settings.ClearsOld[mode]);
+            var info = GetStats();
+            GlobalRankBox.Text = info["global_rank"];
+            GlobalRankGainBox.Text = info["global_rank_change"];
+            CountryRankBox.Text = info["country_rank"];
+            CountryRankGainBox.Text = info["country_rank_change"];
+            RankedScoreBox.Text = info["ranked_score"];
+            RankedScoreGainBox.Text = info["ranked_score_change"];
+            TotalScoreBox.Text = info["total_score"];
+            TotalScoreGainBox.Text = info["total_score_change"];
+            PpBox.Text = info["pp"];
+            PpGainBox.Text = info["pp_change"];
+            AccuracyBox.Text = info["accuracy"];
+            AccuracyGainBox.Text = info["accuracy_change"];
+            PlayTimeBox.Text = info["playtime"];
+            PlayTimeGainBox.Text = info["playtime_change"];
+            PlayCountBox.Text = info["playcount"];
+            PlayCountGainBox.Text = info["playcount_change"];
+            TotalHitsBox.Text = info["total_hits"];
+            TotalHitsGainBox.Text = info["total_hits_change"];
+            ClearsBox.Text = info["clears"];
+            ClearsBoxGain.Text = info["clears_change"];
             if (settings.ScoreRank != null) {
-                ScoreRankBox.Text = FormatNumber(settings.ScoreRank[mode]);
-                ScoreRankGainBox.Text = FormatGainInt(settings.ScoreRank[mode], settings.ScoreRankOld[mode]);
+                ScoreRankBox.Text = info["score_rank"];
+                ScoreRankGainBox.Text = info["score_rank_change"];
             }
             if (settings.Firsts != null && settings.FirstsOld != null) {
-                FirstsBox.Text = FormatNumber(settings.Firsts[mode]);
-                FirstsBoxGain.Text = FormatGainInt(settings.Firsts[mode], settings.FirstsOld[mode]);
+                FirstsBox.Text = info["firsts"];
+                FirstsBoxGain.Text = info["firsts_change"];
             }
             if (settings.NextScoreRank != null) {
-                NextScoreRankBox.Text = FormatNumber(settings.NextScoreRank[mode]);
-                NextScoreRankGain.Text = FormatNumber(settings.NextScoreRank[mode] - new_stats[mode].RankedScore);
+                NextScoreRankBox.Text = info["next_rank_score"];
+                NextScoreRankGain.Text = info["next_rank_score_needed"];
             }
             if (settings.NextPPRank != null) {
-                NextPPRankBox.Text = FormatNumber(settings.NextPPRank[mode]);
-                NextPPRankGain.Text = FormatNumber(settings.NextPPRank[mode] - new_stats[mode].Pp);
+                NextPPRankBox.Text = info["next_rank_pp"];
+                NextPPRankGain.Text = info["next_rank_pp_needed"];
             }
-            SSHBox.Text = FormatNumber(new_stats[mode].Grades.XhCount);
-            SSHGainBox.Text = FormatGainInt(new_stats[mode].Grades.XhCount, old_stats[mode].Grades.XhCount);
-            SSBox.Text = FormatNumber(new_stats[mode].Grades.XCount);
-            SSGainBox.Text = FormatGainInt(new_stats[mode].Grades.XCount, old_stats[mode].Grades.XCount);
-            SHBox.Text = FormatNumber(new_stats[mode].Grades.ShCount);
-            SHGainBox.Text = FormatGainInt(new_stats[mode].Grades.ShCount, old_stats[mode].Grades.ShCount);
-            SBox.Text = FormatNumber(new_stats[mode].Grades.SCount);
-            SGainBox.Text = FormatGainInt(new_stats[mode].Grades.SCount, old_stats[mode].Grades.SCount);
-            ABox.Text = FormatNumber(new_stats[mode].Grades.ACount);
-            AGainBox.Text = FormatGainInt(new_stats[mode].Grades.ACount, old_stats[mode].Grades.ACount);
-            BBox.Text = FormatNumber(new_stats[mode].Grades.BCount);
-            BGainBox.Text = FormatGainInt(new_stats[mode].Grades.BCount, old_stats[mode].Grades.BCount);
-            CBox.Text = FormatNumber(new_stats[mode].Grades.CCount);
-            CGainBox.Text = FormatGainInt(new_stats[mode].Grades.CCount, old_stats[mode].Grades.CCount);
-            DBox.Text = FormatNumber(new_stats[mode].Grades.DCount);
-            DGainBox.Text = FormatGainInt(new_stats[mode].Grades.DCount, old_stats[mode].Grades.DCount);
+            SSHBox.Text = info["ssh"];
+            SSHGainBox.Text = info["ssh_change"];
+            SSBox.Text = info["ss"];
+            SSGainBox.Text = info["ss_change"];
+            SHBox.Text = info["sh"];
+            SHGainBox.Text = info["sh_change"];
+            SBox.Text = info["s"];
+            SGainBox.Text = info["s_change"];
+            ABox.Text = info["a"];
+            AGainBox.Text = info["a_change"];
+            BBox.Text = info["b"];
+            BGainBox.Text = info["b_change"];
+            CBox.Text = info["c"];
+            CGainBox.Text = info["c_change"];
+            DBox.Text = info["d"];
+            DGainBox.Text = info["d_change"];
             InfoLabel.Text = $"{response_new.Username}'s {modes[mode]} session.";
         }
 
